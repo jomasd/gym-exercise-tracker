8f4d0a359b94b2845a8b9c72daeac6a7e8d47b9c
jomasd <jdlopes06@gmail.com>
2023-07-13 08:57:43 -0400

    workout

--- gym-exercise-tracker/imports/ui/pages/HomePage.js
+++ gym-exercise-tracker/imports/ui/pages/HomePage.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const HomePage = () => {
   return (
     <div>
       <h1>Welcome to Exercise Tracker</h1>
+      <nav>
+        <ul>
+          <li><Link to="/exercises">View Exercises</Link></li>
+          <li><Link to="/sets">View Sets</Link></li>
+          <li><Link to="/workoutLists">View Workout Lists</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 };
-
-
