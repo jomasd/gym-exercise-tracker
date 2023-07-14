2f7f36b307067ef159178911ef99bbdc145da444
jomasd <jdlopes06@gmail.com>
2023-07-14 18:51:45 -0400

    workout structure error

--- gym-exercise-tracker/imports/ui/pages/WorkoutListsPage.js
+++ gym-exercise-tracker/imports/ui/pages/WorkoutListsPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import { WorkoutLists } from '../../api/workoutLists/WorkoutListsCollection';
+import { WorkoutLists } from '../../api/workout/WorkoutCollection';
 import WorkoutListList from '../components/WorkoutListList';
 
 const WorkoutListsPage = ({ workoutLists }) => {
@@ -16,6 +16,6 @@ const WorkoutListsPage = ({ workoutLists }) => {
 export default withTracker(() => {
   Meteor.subscribe('workoutLists');
   return {
-    workoutLists: WorkoutLists.find().fetch(),
+    workouts: Workout.find().fetch(),
   };
 })(WorkoutListsPage);
