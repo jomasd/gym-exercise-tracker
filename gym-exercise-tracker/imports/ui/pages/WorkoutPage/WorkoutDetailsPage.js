1b8e6f0e940f0be41c9d3aabdeb1731e1e71ef64
jomasd <jdlopes06@gmail.com>
2023-07-15 11:56:07 -0400

    WorkoutDetailsPage

--- gym-exercise-tracker/imports/ui/pages/WorkoutPage/WorkoutDetailsPage.js
+++ gym-exercise-tracker/imports/ui/pages/WorkoutPage/WorkoutDetailsPage.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { withTracker } from 'meteor/react-meteor-data';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Workouts } from '../../../api/workouts/WorkoutsCollection';
-import { Workout } from '../../components/Workout';
 
-const WorkoutDetailsPage = ({ workout }) => (
-  <div className="workout-details-page">
-    <h1>Workout Details</h1>
-    {workout ? <Workout workout={workout} /> : <p>Loading...</p>}
-  </div>
-);
 
-export default withTracker(() => {
-  const { id } = useParams();
-  Meteor.subscribe('workouts');
+export const WorkoutDetailsPage = () => {
+  const { workoutId } = useParams();
+  const {workout} = useTracker(() => {
+    Meteor.subscribe('workouts');
+    const workout = Workouts.findOne(workoutId);
+    return{workout};
+  });
 
-  return {
-    workout: Workouts.findOne(id),
-  };
-})(WorkoutDetailsPage);
+  return (
+    <>
+    <div className="workout-details-page">
+      <h1>Workout Details</h1>
+      {workout.name}
+      {workout.exercises}
+    </div>
+    </>
+  );
+};
+
+
