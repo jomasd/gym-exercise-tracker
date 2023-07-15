52de99afab8c90a83897ee723d0c8d847abbdd3c
jomasd <jdlopes06@gmail.com>
2023-07-15 15:43:21 -0400

    work dummy data

--- gym-exercise-tracker/imports/startup/server/workouts.js
+++ gym-exercise-tracker/imports/startup/server/workouts.js
@@ -1,39 +1,51 @@
+import { Meteor } from 'meteor/meteor';
 import { Workouts } from '../../api/workouts/WorkoutsCollection';
 import { Exercises } from '../../api/exercises/ExercisesCollection';
+Meteor.startup(() => {
+  if (Workouts.find().count() === 2) {
+    const benchPress = Exercises.findOne({ name: 'Bench Press' });
+    const squat = Exercises.findOne({ name: 'Squat' });
+    const workouts = [
+      {
+        name: 'Workout 1',
+        userId: 'user1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        exercises: [
+          {
+            exerciseId: benchPress._id,
+            recommendedSets: 3,
+            recommendedReps: 10,
+          },
+          {
+            exerciseId: benchPress._id,
+            recommendedSets: 3,
+            recommendedReps: 10,
+          },
+        ],
+      },
+      {
+        name: 'Workout 2',
+        userId: 'user2',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        exercises: [
+          {
+            exerciseId: benchPress._id,
+            recommendedSets: 3,
+            recommendedReps: 10,
+          },
+          {
+            exerciseId: benchPress._id,
+            recommendedSets: 3,
+            recommendedReps: 10,
+          },
+        ],
+      },
+    ];
 
-// If the WorkoutLists collection is empty, add some dummy data.
-const benchPress = Exercises.findOne({ name: 'Bench Press' });
-const squat = Exercises.findOne({ name: 'Squat' });
-if (Workouts.find().count() === 0 && benchPress && squat) {
-  console.log('Inserting dummy workouts...');
-
-  Workouts.insert({
-    name: 'Workout List 1',
-    userId: 'user1',
-    exercises: [benchPress._id, squat._id],
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }, (error, result) => {
-    if (error) {
-      console.error('Error inserting Workout List 1:', error);
-    } else {
-      console.log('Workout List 1 inserted successfully:', result);
-    }
-  });
-
-  Workouts.insert({
-    name: 'Workout List 2',
-    userId: 'user1',
-    exercises: [squat._id],
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }, (error, result) => {
-    if (error) {
-      console.error('Error inserting Workout List 2:', error);
-    } else {
-      console.log('Workout List 2 inserted successfully:', result);
-    }
-  });
-} else {
-  console.log('Skipping insertion of dummy workouts:', Workouts.find().count(), benchPress, squat);
-}
+    workouts.forEach((workout) => {
+      Workouts.insert(workout);
+    });
+  }
+});
